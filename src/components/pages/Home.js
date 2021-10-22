e912ff4fdc8cd4b9df76fff0849f78d7ae005675
Daniel-MSoares <munizsoaresrapper@gmail.com>
2021-10-22 13:50:53 -0300

    Cria page Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,15 @@
+import LinkButton from '../layout/LinkButton'
+
+import styles from'./Home.module.css'
+import savings from'../../img/savings.svg'
 function Home(){
-  return <><h1>HOME</h1></>
+  return (
+       <section className={styles.home_container}>
+         <h1>Wellcome to <span>costs</span></h1>
+         <p>Start manager yours projects just now!</p>
+         <LinkButton to='/newproject' text='Create Project'/>
+         <img src={savings} alt='Costs'/>
+       </section>
+  )
 }
 export default Home
