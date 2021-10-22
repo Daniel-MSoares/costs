024a9a7e83c845847a505ab808b8ab863aa2e951
Daniel-MSoares <munizsoaresrapper@gmail.com>
2021-10-22 14:22:35 -0300

    page newproject e começa criar o projectForm

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,4 +1,12 @@
+import ProjectForm from '../ProjectForm'
+import styles from './NewProject.module.css'
 function NewProject(){
-    return <><h1>NEW PROJECT</h1></>
+    return (
+    <div className={styles.newproject_container}>
+       <h1>Create New Project</h1>
+       <p>Create your project for then add services</p>
+       <ProjectForm/>
+    </div>
+    )
   }
 export default NewProject
