521eb85e5bf1d6facb91a9dc08888aacfd78b0d9
Daniel-MSoares <munizsoaresrapper@gmail.com>
2021-10-28 17:10:22 -0300

    setup pra criar odashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,18 +1,30 @@
 import {useLocation} from "react-router-dom"
 
+import LinkButton from "../layout/LinkButton"
+import Container from  "../layout/Container"
 import Message from "../layout/Message"
-
+import styles from './Projects.module.css'
 function Projects(){
      const location=useLocation()
      let message=''
      if(location.state){
       message=location.state.message
      }
-
     return (
-    <div>
-        <h1>MY PROJECTS</h1>
-        {message && <Message type='success' msg={message} />}
-    </div>)
+    <div className={styles.project_container}>
+        <div className={styles.title_container}>
+          <h1>MY PROJECTS</h1>
+          <LinkButton to="#" text="New Project"/>
+       </div>
+          {message && <Message type='success' msg={message} />}
+       <Container customClass='start'>
+
+         <p>projects...</p>
+
+
+       </Container>
+
+    </div>
+    )
   }
   export default Projects
